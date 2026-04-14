db6dd4b014cb82616bf70a79fc73d5964d2800f4
dilarag3 <dlrgabdullina280706@gmail.com>
2026-04-14 16:40:31 +0500

    Update MyList.java

--- MyList.java
+++ MyList.java
@@ -1,5 +1,4 @@
-// list interface with basic operations
-public interface MyList<T> {
+    public interface MyList<T> {
     void add(T element);
     void add(int index, T element);
     T get(int index);
